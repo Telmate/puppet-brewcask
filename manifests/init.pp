5af865dd85e8f2fccbc319c0eec4f0ec0d0ac889
Erik Petersen <erik@telmate.com>
2013-11-13 13:12:14 +0800

    use telmate forked brewcask

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,9 @@
 class brewcask {
   require homebrew
 
-  homebrew::tap { 'phinze/homebrew-cask': }
+  homebrew::tap { 'Telmate/homebrew-cask': }
    
   package { 'brew-cask':
-    require => Homebrew::Tap['phinze/homebrew-cask']
+    require => Homebrew::Tap['Telmate/homebrew-cask']
   }
 }
